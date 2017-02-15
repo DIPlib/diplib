f406e62957e772105ce667bafb5953a27d04195e
Cris Luengo <cluengo@flagshipbio.com>
2017-02-15 12:06:01 -0700

    Huge speed gain using a `std::map` over a `std::vector` to look up labels.

--- src/measurement/image_chain_code.cpp
+++ src/measurement/image_chain_code.cpp
@@ -8,6 +8,7 @@
 
 
 #include <array>
+#include <map>
 
 #include "diplib.h"
 #include "diplib/chain_code.h"
@@ -18,6 +19,8 @@
 namespace dip {
 
 
+namespace {
+
 struct FreemanCode {
    VertexInteger pos;
    dip::sint offset;
@@ -25,17 +28,20 @@ struct FreemanCode {
 
 using FeemanCodeTable = std::array< FreemanCode, 8 >;
 
+struct ObjectData { dip::uint index; bool done; };
+using ObjectIdList = std::map< dip::uint, ObjectData >; // key is the objectID (label)
+
 template< typename DIP_TPI >
 static ChainCodeArray dip__ChainCode(
       Image const& labels,
-      UnsignedArray const& objectIDs,
+      ObjectIdList& objectIDs,
+      dip::uint nObjects, // potentially different from the largest index in objectIDs, if there were repeated elements in the original list.
       dip::uint connectivity,
       FeemanCodeTable const& freeman
 ) {
-   DIP_ASSERT( labels.DataType() == DataType( DIP_TPI( 0 )));
-   DIP_TPI *data = static_cast< DIP_TPI* >( labels.Origin() );
-   BooleanArray done( objectIDs.size(), false ); // mark labels as done
-   ChainCodeArray ccArray( objectIDs.size() );  // output array
+   DIP_ASSERT( labels.DataType() == DataType( DIP_TPI( 0 ) ) );
+   DIP_TPI* data = static_cast< DIP_TPI* >( labels.Origin() );
+   ChainCodeArray ccArray( nObjects );  // output array
    IntegerArray dims( 2 );
    dims[ 0 ] = labels.Size( 0 ) - 1;
    dims[ 1 ] = labels.Size( 1 ) - 1; // our local copy of `dims` now contains the largest coordinates
@@ -49,17 +55,15 @@ static ChainCodeArray dip__ChainCode(
       for( coord.x = 0; coord.x < dims[ 0 ]; ++coord.x ) {
          bool process = false;
          dip::uint index = 0;
-         if( data[ pos ] != label || coord.x == 0 ) {
-            // Check whether data[pos] is start of not processed object
-            for( index = 0; index < objectIDs.size(); ++index ) {
-               if( data[ pos ] == objectIDs[ index ] ) {
-                  if( !done[ index ] ) {
-                     done[ index ] = true;
-                     label = objectIDs[ index ];
-                     process = true;
-                  }
-                  break;
-               }
+         dip::uint newlabel = data[ pos ];
+         if( ( newlabel != 0 ) && ( newlabel != label ) ) {
+            // Check whether newlabel is start of not processed object
+            auto it = objectIDs.find( newlabel );
+            if( ( it != objectIDs.end() ) && !it->second.done ) {
+               label = newlabel;
+               it->second.done = true;
+               index = it->second.index;
+               process = true;
             }
          }
          if( process ) {
@@ -76,9 +80,9 @@ static ChainCodeArray dip__ChainCode(
 
                VertexInteger nc = c + freeman[ dir ].pos;
                dip::sint no = offset + freeman[ dir ].offset;
-               if(( nc.x >= 0 ) && ( nc.x <= dims[ 0 ] ) &&
-                  ( nc.y >= 0 ) && ( nc.y <= dims[ 1 ] ) &&
-                  (( data[ no ] ) == label )) {
+               if( ( nc.x >= 0 ) && ( nc.x <= dims[ 0 ] ) &&
+                   ( nc.y >= 0 ) && ( nc.y <= dims[ 1 ] ) &&
+                   ( ( data[ no ] ) == label ) ) {
 
                   // Add new chain
                   bool border = ( nc.x == 0 ) || ( nc.x == dims[ 0 ] ) ||
@@ -107,7 +111,7 @@ static ChainCodeArray dip__ChainCode(
 
                }
 
-            } while( !(( c == coord ) && ( dir == 0 )));
+            } while( !( ( c == coord ) && ( dir == 0 ) ) );
          }
          pos += strides[ 0 ];
       }
@@ -115,6 +119,7 @@ static ChainCodeArray dip__ChainCode(
    return ccArray;
 }
 
+} // namespace
 
 ChainCodeArray GetImageChainCodes(
       Image const& labels,
@@ -149,14 +154,28 @@ ChainCodeArray GetImageChainCodes(
       freeman[ 7 ].pos.x =  1; freeman[ 7 ].pos.y =  1; freeman[ 7 ].offset =  xS + yS;
    }
 
+   // Create a map for the object IDs
+   ObjectIdList objectIdList;
+   dip::uint nObjects;
+   if (objectIDs.empty()) {
+      UnsignedArray allObjectIDs = GetObjectLabels( labels, Image(), false );
+      for( dip::uint ii = 0; ii < allObjectIDs.size(); ++ii ) {
+         objectIdList.emplace( allObjectIDs[ ii ], ObjectData{ ii, false } );
+      }
+      nObjects = allObjectIDs.size();
+   } else {
+      for( dip::uint ii = 0; ii < objectIDs.size(); ++ii ) {
+         objectIdList.emplace( objectIDs[ ii ], ObjectData{ ii, false } );
+      }
+      nObjects = objectIDs.size();
+   }
+
    // Get the chaincode for each label
    ChainCodeArray ccArray;
-   DIP_OVL_CALL_ASSIGN_UINT( ccArray, dip__ChainCode, (
-               labels,
-               objectIDs.empty() ? GetObjectLabels( labels, Image(), false ) : objectIDs,
-               connectivity,
-               freeman
-         ), labels.DataType() );
+   DIP_OVL_CALL_ASSIGN_UINT( ccArray,
+                             dip__ChainCode,
+                             ( labels, objectIdList, nObjects, connectivity, freeman ),
+                             labels.DataType() );
    return ccArray;
 }
 
