790c7cf0130d43628eadf78a17a112d6c05d985d
Cris Luengo <cris.l.luengo@gmail.com>
2017-03-15 22:29:16 -0600

    Found a tool to count how many documented things there are in the library.

---
+++ tools/count_functions.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Note: run from ./target/ subdirectory of the main repository
+# (TODO: fix so this is not a requirement)
+
+sed  -e 's|@DOCUMENTATION_OUTPUT@|.|g' \
+     -e 's|@CMAKE_CURRENT_SOURCE_DIR@|..|g' \
+     -e 's|GENERATE_HTML *= YES|GENERATE_HTML = NO|' \
+     -e 's|GENERATE_XML *= NO|GENERATE_XML = YES|' \
+      < ../src/documentation/Doxyfile.in > doxy-coverage.conf
+
+doxygen doxy-coverage.conf
+doxy-coverage.py xml
