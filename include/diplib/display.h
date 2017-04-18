1f832aa1e62b41e99bb56741b016241ab60e1577
Cris Luengo <cluengo@flagshipbio.com>
2017-04-18 15:30:18 -0600

    Ideas on how to implement image display functionality.

--- include/diplib/display.h
+++ include/diplib/display.h
@@ -26,10 +26,48 @@
 
 namespace dip {
 
-// TODO: separate this function into various modules:
-//   - Compute max/min given selected slice, min-max/percentile, etc.
-//   - Extract 2D slice from nD image
-//   - Scale slice and produce 2D uint8 image
+
+// TODO: ImageDisplay to be a class
+//   - It holds a copy of the image, so we're not dependent on the original image still existing. This is where data
+//     is fetched when slice mode, direction or location is changed.
+//   - It holds the 2D slice to be displayed (could be either shared data with the image, or in case of a projection,
+//     owning its own data). This is where intensity lookup is performed.
+//   - It holds another 2D slice, either identical to the previous, or converted to RGB if it was a color image.
+//     This prevents repeated (possibly expensive) conversion to RGB when changing mapping modes.
+//   - It holds a 2D UINT8 image (output). Must be able to set the external interface! The external interface controls
+//     how the color channel dimension is configured.
+//   - It holds display flags, and methods to change flags. Changing the flag does not cause immediate update of output.
+//       - projection/slice direction
+//       - projection/slice mode
+//       - complex->real mapping
+//       - intensity mapping (linear, logarithmic, based, and limits)
+//   - Each of the stored images has a "dirty" flag. Changing display flags causes one or more "dirty" flags to be set.
+//   - It holds information about the image:
+//       - max and min intensity, 5 and 95 percentiles, etc.
+//       - per complex mapping mode
+//       - for the current slice and for the image as a whole (global stretch)
+//       - only stores the stuff that was needed, don't compute things prematurely!
+//   - A method to get the output, which causes updates of (intermediate and output) images flagged as dirty.
+//   - A method to get input image intensity at a given 2D point (automatically finds corresponding 3D location).
+// By sticking all this functionality in DIPlib, and removing it from DIPimage, we'll improve performance, and
+// we'll prevent code duplication when we need this functionality for a Python image display or some other display.
+//
+// Note that for MATLAB it is probably advantageous to use a color map in the display itself, but we could still
+// add a color mapping to this class, where output has a color channel
+//
+// The MATLAB interface will contain a MATLAB handle class with a pointer to the C++ object stored in a MEX-file.
+// The MEX-file will be locked in memory. Instead of pointer, the handle class could have an ID, which is used
+// in a std::map to get the dip::ImageDisplay object (safer?). Each image display window will store this handle
+// object in its UserData.
+//
+// imagedisplay.mex:
+//    handle = imagedisplay(image)
+//    imagedisplay(handle, 'mappingmode', 'lin')
+//    mode = imagedisplay(handle, 'mappingmode')
+//           also: 'globalstretch', 'complexmapping', 'slicemode', 'slicedirection', 'coordinates'
+//    out = imagedisplay(handle)
+//    value = imagedisplay(handle, coords) % using 2D coords, automatically translated
+
 
 /// \brief Parameters to the `dip::ImageDisplay` function.
 struct DIP_NO_EXPORT ImageDisplayParams {
