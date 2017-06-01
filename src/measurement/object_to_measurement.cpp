5a802ccb53a3288af883a30ae59847c6c9da2d77
Cris Luengo <cluengo@flagshipbio.com>
2017-06-01 11:42:10 -0600

    Implemented `dip::ObjectToMeasurement`.

--- src/measurement/object_to_measurement.cpp
+++ src/measurement/object_to_measurement.cpp
@@ -18,18 +18,44 @@
  * limitations under the License.
  */
 
+#include <algorithm>
+
 #include "diplib.h"
 #include "diplib/measurement.h"
+#include "diplib/lookup_table.h"
 
 namespace dip {
 
 
 void ObjectToMeasurement(
-      Image const& /*label*/,
-      Image& /*out*/,
-      Measurement::IteratorFeature const& /*featureValues*/
+      Image const& label,
+      Image& out,
+      Measurement::IteratorFeature const& featureValues
 ) {
-   // TODO, requires LUT function.
+   DIP_THROW_IF( !label.IsScalar(), E::IMAGE_NOT_SCALAR );
+   DIP_THROW_IF( !label.DataType().IsUInt(), E::DATA_TYPE_NOT_SUPPORTED );
+   UnsignedArray const& objects = featureValues.Objects();
+   dip::uint maxObject = *std::max_element( objects.begin(), objects.end() );
+   dip::uint nElements = featureValues.NumberOfValues();
+   bool protect = out.IsProtected();
+   if( !protect ) {
+      // If the user didn't protect the output image, we set it to SFLOAT, which is the default output type
+      out.ReForge( label.Sizes(), nElements, DT_SFLOAT );
+      out.Protect();
+   }
+   Image lutIm( { maxObject + 1 }, nElements, DT_DFLOAT );
+   lutIm.Fill( 0.0 );
+   DIP_ASSERT( lutIm.TensorStride() == 1 );
+   dfloat* data = static_cast< dfloat* >( lutIm.Origin() );
+   dip::sint stride = lutIm.Stride( 0 );
+   auto it = featureValues.FirstObject();
+   while( it ) {
+      dfloat* dest = data + static_cast<dip::sint>( it.ObjectID() ) * stride;
+      std::copy( it.begin(), it.end(), dest );
+   }
+   LookupTable lut( lutIm );
+   lut.Apply( label, out );
+   out.Protect( protect );
 }
 
 
