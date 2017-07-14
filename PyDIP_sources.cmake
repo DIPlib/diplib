aff74c03df4ab0a1f3002eff0a0d52917437ad17
Cris Luengo <cris.l.luengo@gmail.com>
2017-07-14 16:22:08 -0600

    Tracking source files automatically.
    This commit avoids `git pull; make` giving errors because of new source files.

---
+++ PyDIP_sources.cmake
@@ -0,0 +1,1 @@
+# Automatically generated, don't edit! -- pydip/PyDIP_py.py;pydip/image.cpp;pydip/linear.cpp /home/cris/SW/SOUP/newdip/pydip/pydip.h /home/cris/SW/SOUP/newdip/pydip/__init__.py;pydip/math.cpp;pydip/morphology.cpp;pydip/pydip.cpp
