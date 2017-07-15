d136b4c20faf194447a0557859883f5e73ad6e29
Cris Luengo <cris.l.luengo@gmail.com>
2017-07-15 00:08:54 -0600

    Automated tracking of sources much improved (and actually works correctly now too!)

--- DIPviewer_sources.cmake
+++ DIPviewer_sources.cmake
@@ -1,1 +1,19 @@
-# Automatically generated, don't edit! -- viewer/include/diplib/viewer/control.h;viewer/include/diplib/viewer/glfw.h;viewer/include/diplib/viewer/glut.h;viewer/include/diplib/viewer/histogram.h;viewer/include/diplib/viewer/image.h;viewer/include/diplib/viewer/manager.h;viewer/include/diplib/viewer/tensor.h;viewer/include/diplib/viewer/viewer.h;viewer/src/control.cpp;viewer/src/histogram.cpp;viewer/src/image.cpp;viewer/src/manager.cpp;viewer/src/manager/glut.cpp /home/cris/SW/SOUP/newdip/viewer/include/diplib/viewer/slice.h;viewer/src/slice.cpp;viewer/src/tensor.cpp
+# Automatically generated, don't edit!
+set(DIPviewer_sources_bogus
+viewer/include/diplib/viewer/control.h
+viewer/include/diplib/viewer/glfw.h
+viewer/include/diplib/viewer/glut.h
+viewer/include/diplib/viewer/histogram.h
+viewer/include/diplib/viewer/image.h
+viewer/include/diplib/viewer/manager.h
+viewer/include/diplib/viewer/slice.h
+viewer/include/diplib/viewer/tensor.h
+viewer/include/diplib/viewer/viewer.h
+viewer/src/control.cpp
+viewer/src/histogram.cpp
+viewer/src/image.cpp
+viewer/src/manager.cpp
+viewer/src/manager/glfw.cpp
+viewer/src/slice.cpp
+viewer/src/tensor.cpp
+)
--- PyDIP_sources.cmake
+++ PyDIP_sources.cmake
@@ -1,1 +1,11 @@
-# Automatically generated, don't edit! -- pydip/PyDIP_py.py;pydip/image.cpp;pydip/linear.cpp /home/cris/SW/SOUP/newdip/pydip/pydip.h /home/cris/SW/SOUP/newdip/pydip/__init__.py;pydip/math.cpp;pydip/morphology.cpp;pydip/pydip.cpp
+# Automatically generated, don't edit!
+set(PyDIP_sources_bogus
+pydip/PyDIP_py.py
+pydip/__init__.py
+pydip/image.cpp
+pydip/linear.cpp
+pydip/math.cpp
+pydip/morphology.cpp
+pydip/pydip.cpp
+pydip/pydip.h
+)
