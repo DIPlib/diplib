a1948a9c33d88e9f9aace00f4ebcb6246620d309
Wouter Caarls <wouter@caarls.org>
2017-09-26 22:40:35 -0300

    PyDIP.viewer module

---
+++ PyDIPviewer_sources.cmake
@@ -0,0 +1,4 @@
+# Automatically generated, don't edit!
+set(PyDIPviewer_sources_bogus
+pydip/viewer/viewer.cpp
+)
---
+++ pydip/viewer/viewer.cpp
@@ -0,0 +1,30 @@
+/*
+ * PyDIP 3.0, Python bindings for DIPlib 3.0 viewer
+ *
+ * (c)2017, Flagship Biosciences, Inc., written by Cris Luengo.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "diplib.h"
+#include "dipviewer.h"
+
+#include <pybind11/pybind11.h>
+
+using namespace pybind11::literals;
+
+PYBIND11_MODULE( viewer, m ) {
+   m.def( "Show", &dip::viewer::Show, "in"_a, "title"_a = "");
+   m.def( "Draw", &dip::viewer::Draw );
+   m.def( "Spin", &dip::viewer::Spin );
+}
