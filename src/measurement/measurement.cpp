844780c624571e67632ab85ba38ec5199d5f6568
Cris Luengo <cris.l.luengo@gmail.com>
2017-10-03 15:36:10 -0600

    Corrected first column width in `dip::Measurement` printing to stdout.

--- src/measurement/measurement.cpp
+++ src/measurement/measurement.cpp
@@ -36,7 +36,7 @@ std::ostream& operator<<(
    constexpr int separatorWidth = 3;
    constexpr int minimumColumnWidth = 10; // we format numbers with at least this many spaces: '-' + 4 digits of precision + '.' + 'e+NN'
    dip::uint maxID = *std::max_element( msr.Objects().begin(), msr.Objects().end() );
-   const int firstColumnWidth = int( std::ceil( std::log10( maxID ) ) );
+   const int firstColumnWidth = maxID > 0 ? static_cast< int >( std::floor( std::log10( maxID ))) + 1 : 1;
    auto const& values = msr.Values();
    std::vector< int > valueWidths( values.size(), 0 );
    for( dip::uint ii = 0; ii < values.size(); ++ii ) {
@@ -52,7 +52,7 @@ std::ostream& operator<<(
       for( dip::uint jj = 1; jj < features[ ii ].numberValues; ++jj ) {
          featureWidths[ ii ] += valueWidths[ features[ ii ].startColumn + jj ] + separatorWidth;
       }
-      featureWidths[ ii ] = std::max( featureWidths[ ii ], int( features[ ii ].name.size() ));
+      featureWidths[ ii ] = std::max( featureWidths[ ii ], static_cast< int >( features[ ii ].name.size() ));
    }
    // Write out the header: feature names
    os << std::setw( firstColumnWidth ) << " " << " | ";
