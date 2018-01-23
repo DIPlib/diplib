fcb69cc0b258121096e6f96e788237ee4d559dca
Cris Luengo <cris.l.luengo@gmail.com>
2018-01-22 17:21:15 -0700

    `fileviewer` example program shows information about the image read.

--- examples/fileviewer.cpp
+++ examples/fileviewer.cpp
@@ -6,35 +6,28 @@
 
 #include <dipviewer.h>
 
-int main(int argc, char **argv) {
-   if (argc < 2)
-   {
+int main( int argc, char** argv ) {
+   if( argc < 2 ) {
       std::cerr << "Usage: fileviewer <image> [image ...]" << std::endl;
       return 1;
-   }
-   else
-   {
-      for (size_t ii=1; (int)ii < argc; ++ii)
-      {
-         std::string arg(argv[ii]);
-      
-         if ( dip::FileCompareExtension(arg, "ics") )
-         {
-            dip::viewer::Show( dip::ImageReadICS( arg ), arg );
-         }
-         else if ( dip::FileCompareExtension(arg, "tiff") || dip::FileCompareExtension(arg, "tif") )
-         {
-            dip::viewer::Show( dip::ImageReadTIFF( arg ), arg );
-         }
-         else
-         {
-            std::cerr << "Unrecognized image extension " << dip::FileGetExtension(arg) << std::endl;
+   } else {
+      for( size_t ii = 1; ( int )ii < argc; ++ii ) {
+         std::string arg( argv[ ii ] );
+         dip::Image img;
+         if( dip::FileCompareExtension( arg, "ics" )) {
+            img = dip::ImageReadICS( arg );
+         } else if( dip::FileCompareExtension( arg, "tiff" ) || dip::FileCompareExtension( arg, "tif" )) {
+            img = dip::ImageReadTIFF( arg );
+         } else {
+            std::cerr << "Unrecognized image extension " << dip::FileGetExtension( arg ) << std::endl;
             return -1;
          }
+         std::cout << arg << ": " << img;
+         dip::viewer::Show( img, arg );
       }
    }
 
-   dip::viewer::Spin( );
+   dip::viewer::Spin();
 
    return 0;
 }
