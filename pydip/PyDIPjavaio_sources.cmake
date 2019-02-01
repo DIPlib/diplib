2f3931fc34c965ca43ec790cb273be0c22875406
Wouter Caarls <wouter@caarls.org>
2019-02-01 20:25:36 -0200

    DIPjavaio Python bindings (mostly superfluous)

---
+++ pydip/PyDIPjavaio_sources.cmake
@@ -0,0 +1,4 @@
+# Automatically generated, don't edit!
+set(PyDIPjavaio_sources_bogus
+javaio/javaio.cpp
+)
---
+++ pydip/javaio/javaio.cpp
@@ -0,0 +1,27 @@
+/* PyDIP 3.0, Python bindings for DIPlib 3.0 JavaIO
+ *
+ * (c)2017, Wouter Caarls
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "diplib.h"
+#include "diplib/javaio.h"
+
+#include <pybind11/pybind11.h>
+
+using namespace pybind11::literals;
+
+PYBIND11_MODULE( PyDIPjavaio, m ) {
+   m.def( "ImageReadJavaIO", [](dip::String const& filename, dip::String const& interface) { return dip::javaio::ImageReadJavaIO(filename, interface); }, "filename"_a, "interface"_a = "org/diplib/BioFormatsInterface");
+}
