e5fe70179e02046f8f3f52c1796de2b46d5fd4ba
Cris Luengo <cris.l.luengo@gmail.com>
2019-02-04 20:53:51 -0700

    Missed a file in the previous commit.

--- javaio/DIPjavaio_sources.cmake
+++ javaio/DIPjavaio_sources.cmake
@@ -6,8 +6,12 @@ java/Image.java
 java/PhysicalQuantity.java
 java/bioformats/BioFormatsInterface.java
 src/fileinformation.cpp
+src/fileinformation.h
 src/image.cpp
+src/image.h
 src/javaio.cpp
 src/physicalquantity.cpp
+src/physicalquantity.h
 src/tools.cpp
+src/tools.h
 )
