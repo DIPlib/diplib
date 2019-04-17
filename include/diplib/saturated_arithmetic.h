c535c54f64701c49b0df65a9e56c7e10247f9105
Cris Luengo <cris.l.luengo@gmail.com>
2019-04-16 22:43:40 -0600

    Simplified saturated arithmetic functions a bit more, reducing most of the code duplication.

--- include/diplib/saturated_arithmetic.h
+++ include/diplib/saturated_arithmetic.h
@@ -2,7 +2,7 @@
  * DIPlib 3.0
  * This file contains overloaded definitions for the functions dip::saturated_XXX().
  *
- * (c)2016, Cris Luengo.
+ * (c)2016-2019, Cris Luengo.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -56,50 +56,55 @@ namespace dip {
 /// \{
 
 
+namespace detail {
+
+template< typename T > struct LargerType { using type = void; };
+template<> struct LargerType< uint8 > { using type = uint32; };
+template<> struct LargerType< uint16 > { using type = uint32; };
+template<> struct LargerType< uint32 > { using type = uint64; };
+#ifdef __SIZEOF_INT128__
+template<> struct LargerType< uint64 > { using type = __uint128_t; };
+#endif
+
+template<> struct LargerType< sint8 > { using type = sint32; };
+template<> struct LargerType< sint16 > { using type = sint32; };
+template<> struct LargerType< sint32 > { using type = sint64; };
+#ifdef __SIZEOF_INT128__
+template<> struct LargerType< sint64 > { using type = __int128_t; };
+#endif
+
+} // namespace detail
+
+
 //
 // Addition
 //
 
 /// \brief Adds two values using saturated arithmetic.
-// The base template is good for floats and complex.
-template< typename T >
+
+// Floats and complex don't overflow
+template< typename T, typename std::enable_if_t< detail::is_floating_point< T >::value
+                                              || detail::is_complex< T >::value, int > = 0 >
 constexpr inline T saturated_add( T lhs, T rhs ) {
    return lhs + rhs;
 }
 
 // Unsigned integers overflow by giving a result that is smaller than either operand.
 // This code is supposed to be branchless, the compiler optimizes using a conditional move.
-template<>
-constexpr inline uint64 saturated_add( uint64 lhs, uint64 rhs ) {
-   uint64 res = lhs + rhs;
-   return res < lhs ? std::numeric_limits< uint64 >::max() : res;
-}
-template<>
-constexpr inline uint32 saturated_add( uint32 lhs, uint32 rhs ) {
-   uint32 res = lhs + rhs;
-   return res < lhs ? std::numeric_limits< uint32 >::max() : res;
-}
-template<>
-constexpr inline uint16 saturated_add( uint16 lhs, uint16 rhs ) {
-   uint16 res = static_cast< uint16 >( lhs + rhs ); // there's an implicit conversion to `unsigned int`
-   return res < lhs ? std::numeric_limits< uint16 >::max() : res;
-}
-template<>
-constexpr inline uint8 saturated_add( uint8 lhs, uint8 rhs ) {
-   uint8 res = static_cast< uint8 >( lhs + rhs ); // there's an implicit conversion to `unsigned int`
-   return res < lhs ? std::numeric_limits< uint8 >::max() : res;
+template< typename T, typename std::enable_if_t< detail::is_unsigned_integer< T >::value, int > = 0 >
+constexpr inline T saturated_add( T lhs, T rhs ) {
+   T res = static_cast< T >( lhs + rhs ); // There's an implicit conversion to unsigned/int for smaller types
+   return res < lhs ? std::numeric_limits< T >::max() : res;
 }
 
 // Signed integers are more complex, we simply use a larger integer type to do the operation.
-// TODO: overflow only happens if both operands have the same sign, opportunity for improvement?
-#ifdef __SIZEOF_INT128__
-template<>
-constexpr inline sint64 saturated_add( sint64 lhs, sint64 rhs ) {
-   return clamp_cast< sint64 >( static_cast< __int128_t >( lhs ) + static_cast< __int128_t >( rhs ));
+template< typename T, typename std::enable_if_t< detail::is_signed_integer< T >::value, int > = 0 >
+constexpr inline T saturated_add( T lhs, T rhs ) {
+   return clamp_cast< T >( static_cast< typename detail::LargerType< T >::type >( lhs )
+                         + static_cast< typename detail::LargerType< T >::type >( rhs ));
 }
-#else
-// However, if we don't have a 128-bit integer type, we need to do this the hard way
-template<>
+#ifndef __SIZEOF_INT128__
+// If we don't have a 128-bit integer type, we need to do this the hard way
 constexpr inline sint64 saturated_add( sint64 lhs, sint64 rhs ) {
    if(( rhs > 0 ) && ( std::numeric_limits< sint64 >::max() - rhs <= lhs )) {
       return std::numeric_limits< sint64 >::max();
@@ -110,21 +115,8 @@ constexpr inline sint64 saturated_add( sint64 lhs, sint64 rhs ) {
    return lhs + rhs;
 }
 #endif // __SIZEOF_INT128__
-template<>
-constexpr inline sint32 saturated_add( sint32 lhs, sint32 rhs ) {
-   return clamp_cast< sint32 >( static_cast< sint64 >( lhs ) + static_cast< sint64 >( rhs ));
-}
-template<>
-constexpr inline sint16 saturated_add( sint16 lhs, sint16 rhs ) {
-   return clamp_cast< sint16 >( static_cast< sint32 >( lhs ) + static_cast< sint32 >( rhs ));
-}
-template<>
-constexpr inline sint8 saturated_add( sint8 lhs, sint8 rhs ) {
-   return clamp_cast< sint8 >( static_cast< sint32 >( lhs ) + static_cast< sint32 >( rhs ));
-}
 
 // Binary addition is equivalent to OR.
-template<>
 constexpr inline bin saturated_add( bin lhs, bin rhs ) {
    return lhs || rhs;
 }
@@ -135,45 +127,30 @@ constexpr inline bin saturated_add( bin lhs, bin rhs ) {
 //
 
 /// \brief Subtracts two values using saturated arithmetic.
-// The base template is good for floats and complex.
-template< typename T >
+
+// Floats and complex don't overflow
+template< typename T, typename std::enable_if_t< detail::is_floating_point< T >::value
+                                              || detail::is_complex< T >::value, int > = 0 >
 constexpr inline T saturated_sub( T lhs, T rhs ) {
    return lhs - rhs;
 }
 
 // Unsigned integers underflow by giving a result that is larger than either operand.
 // This code is supposed to be branchless, the compiler optimizes using a conditional move.
-template<>
-constexpr inline uint64 saturated_sub( uint64 lhs, uint64 rhs ) {
-   uint64 res = lhs - rhs;
-   return res > lhs ? uint64( 0 ) : res;
-}
-template<>
-constexpr inline uint32 saturated_sub( uint32 lhs, uint32 rhs ) {
-   uint32 res = lhs - rhs;
-   return res > lhs ? uint32( 0 ) : res;
-}
-template<>
-constexpr inline uint16 saturated_sub( uint16 lhs, uint16 rhs ) {
-   uint16 res = static_cast< uint16 >( lhs - rhs ); // there's an implicit conversion to `unsigned int`
-   return res > lhs ? uint16( 0 ) : res;
-}
-template<>
-constexpr inline uint8 saturated_sub( uint8 lhs, uint8 rhs ) {
-   uint8 res = static_cast< uint8 >( lhs - rhs ); // there's an implicit conversion to `unsigned int`
-   return res > lhs ? uint8( 0 ) : res;
+template< typename T, typename std::enable_if_t< detail::is_unsigned_integer< T >::value, int > = 0 >
+constexpr inline T saturated_sub( T lhs, T rhs ) {
+   T res = static_cast< T >( lhs - rhs ); // There's an implicit conversion to unsigned/int for smaller types
+   return res > lhs ? T( 0 ) : res;
 }
 
 // Signed integers are more complex, we simply use a larger integer type to do the operation.
-// TODO: overflow only happens if both operands have the opposite sign, opportunity for improvement?
-#ifdef __SIZEOF_INT128__
-template<>
-constexpr inline sint64 saturated_sub( sint64 lhs, sint64 rhs ) {
-   return clamp_cast< sint64 >( static_cast< __int128_t >( lhs ) - static_cast< __int128_t >( rhs ));
+template< typename T, typename std::enable_if_t< detail::is_signed_integer< T >::value, int > = 0 >
+constexpr inline T saturated_sub( T lhs, T rhs ) {
+   return clamp_cast< T >( static_cast< typename detail::LargerType< T >::type >( lhs )
+                         - static_cast< typename detail::LargerType< T >::type >( rhs ));
 }
-#else
-// However, if we don't have a 128-bit integer type, we need to do this the hard way
-template<>
+#ifndef __SIZEOF_INT128__
+// If we don't have a 128-bit integer type, we need to do this the hard way
 constexpr inline sint64 saturated_sub( sint64 lhs, sint64 rhs ) {
    if(( rhs < 0 ) && ( std::numeric_limits< sint64 >::max() + rhs <= lhs )) {
       return std::numeric_limits< sint64 >::max();
@@ -184,21 +161,8 @@ constexpr inline sint64 saturated_sub( sint64 lhs, sint64 rhs ) {
    return lhs - rhs;
 }
 #endif // __SIZEOF_INT128__
-template<>
-constexpr inline sint32 saturated_sub( sint32 lhs, sint32 rhs ) {
-   return clamp_cast< sint32 >( static_cast< sint64 >( lhs ) - static_cast< sint64 >( rhs ));
-}
-template<>
-constexpr inline sint16 saturated_sub( sint16 lhs, sint16 rhs ) {
-   return clamp_cast< sint16 >( static_cast< sint32 >( lhs ) - static_cast< sint32 >( rhs ));
-}
-template<>
-constexpr inline sint8 saturated_sub( sint8 lhs, sint8 rhs ) {
-   return clamp_cast< sint8 >( static_cast< sint32 >( lhs ) - static_cast< sint32 >( rhs ));
-}
 
 // Binary subtraction is equivalent to AND NOT
-template<>
 constexpr inline bin saturated_sub( bin lhs, bin rhs ) {
    return lhs && !rhs;
 }
@@ -209,21 +173,22 @@ constexpr inline bin saturated_sub( bin lhs, bin rhs ) {
 //
 
 /// \brief Multiplies two values using saturated arithmetic.
-// The base template is good for floats and complex.
-template< typename T >
+
+// Floats and complex don't overflow
+template< typename T, typename std::enable_if_t< detail::is_floating_point< T >::value
+                                              || detail::is_complex< T >::value, int > = 0 >
 constexpr inline T saturated_mul( T lhs, T rhs ) {
    return lhs * rhs;
 }
 
-// For unsigned integers we simply use a larger integer type to do the operation.
-#ifdef __SIZEOF_INT128__
-template<>
-constexpr inline uint64 saturated_mul( uint64 lhs, uint64 rhs ) {
-   return clamp_cast< uint64 >( static_cast< __uint128_t >( lhs ) * static_cast< __uint128_t >( rhs ));
+// For signed and unsigned integers we simply use a larger integer type to do the operation.
+template< typename T, typename std::enable_if_t< detail::is_integer< T >::value, int > = 0 >
+constexpr inline T saturated_mul( T lhs, T rhs ) {
+   return clamp_cast< T >( static_cast< typename detail::LargerType< T >::type >( lhs )
+                         * static_cast< typename detail::LargerType< T >::type >( rhs ));
 }
-#else
+#ifndef __SIZEOF_INT128__
 // However, if we don't have a 128-bit integer type, we need to do this the hard way
-template<>
 constexpr inline uint64 saturated_mul( uint64 lhs, uint64 rhs ) {
    uint64 result = lhs * rhs;
    if(( lhs != 0 ) && ( result / lhs != rhs )) {
@@ -231,29 +196,7 @@ constexpr inline uint64 saturated_mul( uint64 lhs, uint64 rhs ) {
    }
    return result;
 }
-#endif // __SIZEOF_INT128__
-template<>
-constexpr inline uint32 saturated_mul( uint32 lhs, uint32 rhs ) {
-   return clamp_cast< uint32 >( static_cast< uint64 >( lhs ) * static_cast< uint64 >( rhs ));
-}
-template<>
-constexpr inline uint16 saturated_mul( uint16 lhs, uint16 rhs ) {
-   return clamp_cast< uint16 >( static_cast< uint32 >( lhs ) * static_cast< uint32 >( rhs ));
-}
-template<>
-constexpr inline uint8 saturated_mul( uint8 lhs, uint8 rhs ) {
-   return clamp_cast< uint8 >( static_cast< uint32 >( lhs ) * static_cast< uint32 >( rhs ));
-}
 
-// For signed integers we simply use a larger integer type to do the operation.
-#ifdef __SIZEOF_INT128__
-template<>
-constexpr inline sint64 saturated_mul( sint64 lhs, sint64 rhs ) {
-   return clamp_cast< sint64 >( static_cast< __int128_t >( lhs ) * static_cast< __int128_t >( rhs ));
-}
-#else
-// However, if we don't have a 128-bit integer type, we need to do this the hard way
-template<>
 constexpr inline sint64 saturated_mul( sint64 lhs, sint64 rhs ) {
    sint64 result = lhs * rhs;
    if(( lhs != 0 ) && ( result / lhs != rhs )) {
@@ -262,21 +205,8 @@ constexpr inline sint64 saturated_mul( sint64 lhs, sint64 rhs ) {
    return result;
 }
 #endif // __SIZEOF_INT128__
-template<>
-constexpr inline sint32 saturated_mul( sint32 lhs, sint32 rhs ) {
-   return clamp_cast< sint32 >( static_cast< sint64 >( lhs ) * static_cast< sint64 >( rhs ));
-}
-template<>
-constexpr inline sint16 saturated_mul( sint16 lhs, sint16 rhs ) {
-   return clamp_cast< sint16 >( static_cast< sint32 >( lhs ) * static_cast< sint32 >( rhs ));
-}
-template<>
-constexpr inline sint8 saturated_mul( sint8 lhs, sint8 rhs ) {
-   return clamp_cast< sint8 >( static_cast< sint32 >( lhs ) * static_cast< sint32 >( rhs ));
-}
 
 // Binary multiplication is equivalent to AND
-template<>
 constexpr inline bin saturated_mul( bin lhs, bin rhs ) {
    return lhs && rhs;
 }
@@ -287,35 +217,23 @@ constexpr inline bin saturated_mul( bin lhs, bin rhs ) {
 //
 
 /// \brief Divides two values using saturated arithmetic.
-template< typename T >
+
+// Floats, complex and unsigned integers don't overflow
+template< typename T, typename std::enable_if_t< detail::is_floating_point< T >::value
+                                              || detail::is_complex< T >::value
+                                              || detail::is_unsigned_integer< T >::value, int > = 0 >
 constexpr inline T saturated_div( T lhs, T rhs ) {
    return static_cast< T >( lhs / rhs ); // There's an implicit conversion to unsigned/int for smaller types
 }
 
 // Signed integer division can overflow if we divide INT_MIN by -1
-template<>
-constexpr inline sint64 saturated_div( sint64 lhs, sint64 rhs ) {
-   return (( lhs == std::numeric_limits< sint64 >::lowest() ) && ( rhs == -1 ))
-          ? std::numeric_limits< sint64 >::max() : ( lhs / rhs );
-}
-template<>
-constexpr inline sint32 saturated_div( sint32 lhs, sint32 rhs ) {
-   return (( lhs == std::numeric_limits< sint32 >::lowest() ) && ( rhs == -1 ))
-          ? std::numeric_limits< sint32 >::max() : lhs / rhs;
-}
-template<>
-constexpr inline sint16 saturated_div( sint16 lhs, sint16 rhs ) {
-   return (( lhs == std::numeric_limits< sint16 >::lowest() ) && ( rhs == -1 ))
-          ? std::numeric_limits< sint16 >::max() : static_cast< sint16 >( lhs / rhs ); // There's an implicit conversion to unsigned/int for smaller types
-}
-template<>
-constexpr inline sint8 saturated_div( sint8 lhs, sint8 rhs ) {
-   return (( lhs == std::numeric_limits< sint8 >::lowest() ) && ( rhs == -1 ))
-          ? std::numeric_limits< sint8 >::max() : static_cast< sint8 >( lhs / rhs ); // There's an implicit conversion to unsigned/int for smaller types
+template< typename T, typename std::enable_if_t< detail::is_signed_integer< T >::value, int > = 0 >
+constexpr inline T saturated_div( T lhs, T rhs ) {
+   return (( lhs == std::numeric_limits< T >::lowest()) && ( rhs == -1 ))
+          ? std::numeric_limits< T >::max() : static_cast< T >( lhs / rhs ); // There's an implicit conversion to unsigned/int for smaller types
 }
 
 // Binary division is equivalent to OR NOT (just to pick something... is this meaningful?).
-template<>
 constexpr inline bin saturated_div( bin lhs, bin rhs ) {
    return lhs || !rhs;
 }
@@ -324,11 +242,10 @@ constexpr inline bin saturated_div( bin lhs, bin rhs ) {
 /// by zero, return 0 rather than infinity or NaN (or an exception).
 template< typename T >
 constexpr inline T saturated_safediv( T lhs, T rhs ) {
-   return rhs == T( 0 ) ? T( 0 ) : saturated_div< T >( lhs, rhs );
+   return rhs == T( 0 ) ? T( 0 ) : saturated_div( lhs, rhs );
 }
 
 // Binary division doesn't need the test, defer to saturated_div.
-template<>
 constexpr inline bin saturated_safediv( bin lhs, bin rhs ) {
    return saturated_div( lhs, rhs );
 }
@@ -339,49 +256,27 @@ constexpr inline bin saturated_safediv( bin lhs, bin rhs ) {
 //
 
 /// \brief Inverts a value using saturated arithmetic. This is the same as negation, but not for unsigned values.
-// The base template is good for floats and complex.
-template< typename T >
+
+// Floats and complex are straight-forward
+template< typename T, typename std::enable_if_t< detail::is_floating_point< T >::value
+                                                 || detail::is_complex< T >::value, int > = 0 >
 constexpr inline T saturated_inv( T v ) {
    return -v;
 }
 
 // Unsigned integers invert by subtracting from max value.
-template<>
-constexpr inline uint64 saturated_inv( uint64 v ) {
-   return std::numeric_limits< uint64 >::max() - v;
-}
-template<>
-constexpr inline uint32 saturated_inv( uint32 v ) {
-   return std::numeric_limits< uint32 >::max() - v;
-}
-template<>
-constexpr inline uint16 saturated_inv( uint16 v ) {
-   return static_cast< uint16 >( static_cast< uint32 >( std::numeric_limits< uint16 >::max() ) - static_cast< uint32 >( v ));
-}
-template<>
-constexpr inline uint8 saturated_inv( uint8 v ) {
-   return static_cast< uint8 >( static_cast< uint32 >( std::numeric_limits< uint8 >::max() ) - static_cast< uint32 >( v ));
+template< typename T, typename std::enable_if_t< detail::is_unsigned_integer< T >::value, int > = 0 >
+constexpr inline T saturated_inv( T v ) {
+   return static_cast< T >( std::numeric_limits< T >::max() - v ); // There's an implicit conversion to unsigned/int for smaller types
 }
 
 // Signed integers seem simple but overflow can happen if the value is equal to lowest possible value
-template<>
-constexpr inline sint64 saturated_inv( sint64 v ) {
-   return v == std::numeric_limits< sint64 >::lowest() ? std::numeric_limits< sint64 >::max() : -v;
+template< typename T, typename std::enable_if_t< detail::is_signed_integer< T >::value, int > = 0 >
+constexpr inline T saturated_inv( T v ) {
+   return v == std::numeric_limits< T >::lowest() ? std::numeric_limits< T >::max() : static_cast< T >( -v ); // There's an implicit conversion to unsigned/int for smaller types
 }
-template<>
-constexpr inline sint32 saturated_inv( sint32 v ) {
-   return v == std::numeric_limits< sint32 >::lowest() ? std::numeric_limits< sint32 >::max() : -v;
-}
-template<>
-constexpr inline sint16 saturated_inv( sint16 v ) {
-   return v == std::numeric_limits< sint16 >::lowest() ? std::numeric_limits< sint16 >::max() : static_cast< sint16 >( -v );
-}
-template<>
-constexpr inline sint8 saturated_inv( sint8 v ) {
-   return v == std::numeric_limits< sint8 >::lowest() ? std::numeric_limits< sint8 >::max() : static_cast< sint8 >( -v );
-}
+
 // Binary inversion is equivalent to NOT
-template<>
 constexpr inline bin saturated_inv( bin v ) {
    return !v;
 }
