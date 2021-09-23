c684eded75211e365b51337e48e8b86bcc20d5b6
Cris Luengo <cris.l.luengo@gmail.com>
2021-09-23 10:09:17 -0600

    We deploy a release candidate first, so we can test the deployed packages for correctness.

--- tools/count_lines.sh
+++ tools/count_lines.sh
@@ -3,9 +3,10 @@
 # Note: run from any subdirectory of the main repository (for example ./tools/ or ./target/)
 # (TODO: fix so this is not a requirement)
 
-wc ../include/*.h ../include/*/*.h ../include/*/*/*.h ../doc/src/*.md ../src/*/*.h ../src/*/*.cpp \
-   ../viewer/src/*.cpp ../viewer/src/*/*.cpp ../viewer/java/*.java \
-   ../javaio/src/* ../javaio/java/*.java ../javaio/java/*/*.java
+wc ../include/*.h ../include/*/*.h ../include/*/*/*.h ../src/*/*.h ../src/*/*.cpp \
+   ../viewer/src/*.cpp ../viewer/src/*/*.cpp ../viewer/java/*.java ../viewer/dipimage/* \
+   ../javaio/src/* ../javaio/java/*.java ../javaio/java/*/*.java \
+   ../doc/src/*.md ../doc/src/*/*.md
 # Selecting extensions explicitly to exclude non-code files,
 # and the *.hpp files from the PCG library that is in ../include/diplib/private/
 # and the font file that is in ../viewer/src/fg_font_data.h.
