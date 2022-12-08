046f530125cfffa6556199085aff5c57fd036448
Cris Luengo <cris.l.luengo@gmail.com>
2022-12-08 01:14:25 -0700

    Fix Linux deploy. [skip ci]

--- tools/travis/manylinux2010.sh
+++ tools/travis/manylinux2010.sh
@@ -3,8 +3,8 @@
 
 # Setup
 yum -y install wget freeglut-devel java-1.8.0-openjdk-devel.x86_64
-/opt/python/cp39-cp39m/bin/python -m pip install cmake auditwheel
-export CMAKE=/opt/python/cp39-cp39m/lib/python3.9/site-packages/cmake/data/bin/cmake
+/opt/python/cp39-cp39/bin/python -m pip install cmake auditwheel
+export CMAKE=/opt/python/cp39-cp39/lib/python3.9/site-packages/cmake/data/bin/cmake
 export BUILD_THREADS=2
 export AUDITWHEEL=`pwd`/diplib/tools/travis/auditwheel
 
@@ -23,27 +23,27 @@ export PYTHON=cp38-cp38
 export PYTHON_VERSION=3.8
 $CMAKE .. -DPYBIND11_PYTHON_VERSION=$PYTHON_VERSION -DPYTHON_EXECUTABLE=/opt/python/$PYTHON/bin/python
 make -j $BUILD_THREADS bdist_wheel
-/opt/python/cp39-cp39m/bin/python $AUDITWHEEL repair pydip/staging/dist/*.whl
+/opt/python/cp39-cp39/bin/python $AUDITWHEEL repair pydip/staging/dist/*.whl
 
 # Python 3.9
 export PYTHON=cp39-cp39
 export PYTHON_VERSION=3.9
 $CMAKE .. -DPYBIND11_PYTHON_VERSION=$PYTHON_VERSION -DPYTHON_EXECUTABLE=/opt/python/$PYTHON/bin/python
 make -j $BUILD_THREADS bdist_wheel
-/opt/python/cp39-cp39m/bin/python $AUDITWHEEL repair pydip/staging/dist/*.whl
+/opt/python/cp39-cp39/bin/python $AUDITWHEEL repair pydip/staging/dist/*.whl
 
 # Python 3.10
 export PYTHON=cp310-cp310
 export PYTHON_VERSION=3.10
 $CMAKE .. -DPYBIND11_PYTHON_VERSION=$PYTHON_VERSION -DPYTHON_EXECUTABLE=/opt/python/$PYTHON/bin/python
 make -j $BUILD_THREADS bdist_wheel
-/opt/python/cp39-cp39m/bin/python $AUDITWHEEL repair pydip/staging/dist/*.whl
+/opt/python/cp39-cp39/bin/python $AUDITWHEEL repair pydip/staging/dist/*.whl
 
 # Python 3.11
 export PYTHON=cp311-cp311
 export PYTHON_VERSION=3.11
 $CMAKE .. -DPYBIND11_PYTHON_VERSION=$PYTHON_VERSION -DPYTHON_EXECUTABLE=/opt/python/$PYTHON/bin/python
 make -j $BUILD_THREADS bdist_wheel
-/opt/python/cp39-cp39m/bin/python $AUDITWHEEL repair pydip/staging/dist/*.whl
+/opt/python/cp39-cp39/bin/python $AUDITWHEEL repair pydip/staging/dist/*.whl
 
 mv wheelhouse /io
