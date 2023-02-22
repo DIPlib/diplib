324c81a8e6fb9f8c3762aa5c106f1b306428551e
Cris Luengo <cris.l.luengo@gmail.com>
2023-02-22 16:50:28 -0700

    An old MATLAB script needed some fixing up. [skip ci]

--- tools/compare_dipimage.m
+++ tools/compare_dipimage.m
@@ -1,17 +1,20 @@
-n = dir('/Users/cris/newdip/dipimage/*.m');
-n2 = dir('/Users/cris/newdip/dipimage/alias/*.m');
-n = cat(1,n,n2);
+newdip_dir = '/Users/cris/src/diplib';
+olddip_dir = '/Users/cris/src/old_dip';
+
+n = dir(fullfile(newdip_dir, 'dipimage', '*.m'));
+n2 = dir(fullfile(newdip_dir, 'dipimage', 'alias', '*.m'));
+n = cat(1, n, n2);
 n = {n.name}';
 
-o = dir('/Users/cris/dip/diptree/dipimage/*files/*.m');
+o = dir(fullfile(olddip_dir, 'diptree', 'dipimage', '*files', '*.m'));
 o = {o.name}';
 
-added = setdiff(n,o)
-missing = setdiff(o,n)
+added = setdiff(n, o);
+missing = setdiff(o, n);
 
-f = fopen('/Users/cris/newdip/dipimage/updated_TODO','w');
-fprintf(f,'Functions in DIPimage 3 that didn''t exist in DIPimage 2:\n\n');
-fprintf(f,'%s\n',added{:});
-fprintf(f,'\n\n\nFunctions in DIPimage 2 that are not yet ported:\n\n');
-fprintf(f,'%s\n',missing{:});
+f = fopen('TODO.txt', 'w');
+fprintf(f, 'Functions in DIPimage 3 that didn''t exist in DIPimage 2:\n\n');
+fprintf(f, '%s\n', added{:});
+fprintf(f, '\n\n\nFunctions in DIPimage 2 that are not yet ported:\n\n');
+fprintf(f, '%s\n', missing{:});
 fclose(f);
