69577f57a2e28b23edc4684b0a1c715b8faa24d5
Wouter Caarls <wouter@caarls.org>
2024-01-12 14:52:31 -0300

    Cleaned up docker script

--- tools/travis/manylinux.sh
+++ tools/travis/manylinux.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-# Run this in a manylinux2010 docker container with /io mounted to some local directory
+# Run this in a manylinux2014 docker container with /io mounted to some local directory
 
 # Setup
 yum -y install wget freeglut-devel java-1.8.0-openjdk-devel.x86_64
 /opt/python/cp39-cp39/bin/python -m pip install cmake auditwheel
-export CMAKE=/opt/python/cp39-cp39/lib/python3.9/site-packages/cmake/data/bin/cmake
-export BUILD_THREADS=2
-export EXCLUDES="--exclude libjvm.so --exclude libOpenGL.so.0 --exclude libGLX.so.0 --exclude libGLdispatch.so.0"
+CMAKE=/opt/python/cp39-cp39/lib/python3.9/site-packages/cmake/data/bin/cmake
+BUILD_THREADS=2
+PYTHON_VERSIONS=(3.8 3.9 3.10 3.11 3.12)
+EXCLUDES=(libjvm.so libOpenGL.so.0 libGLX.so.0 libGLdispatch.so.0)
 
 # Clone diplib repository
 git clone https://github.com/diplib/diplib
@@ -18,39 +19,11 @@ wget -nv https://downloads.openmicroscopy.org/bio-formats/7.0.0/artifacts/biofor
 # Basic configuration
 $CMAKE .. -DDIP_PYDIP_WHEEL_INCLUDE_LIBS=On -DBIOFORMATS_JAR=`pwd`/bioformats_package.jar 
 
-# Python 3.8
-export PYTHON=cp38-cp38
-export PYTHON_VERSION=3.8
-$CMAKE .. -DPYBIND11_PYTHON_VERSION=$PYTHON_VERSION -DPYTHON_EXECUTABLE=/opt/python/$PYTHON/bin/python
-make -j $BUILD_THREADS bdist_wheel
-auditwheel repair $EXCLUDES pydip/staging/dist/*.whl
-
-# Python 3.9
-export PYTHON=cp39-cp39
-export PYTHON_VERSION=3.9
-$CMAKE .. -DPYBIND11_PYTHON_VERSION=$PYTHON_VERSION -DPYTHON_EXECUTABLE=/opt/python/$PYTHON/bin/python
-make -j $BUILD_THREADS bdist_wheel
-auditwheel repair $EXCLUDES pydip/staging/dist/*.whl
-
-# Python 3.10
-export PYTHON=cp310-cp310
-export PYTHON_VERSION=3.10
-$CMAKE .. -DPYBIND11_PYTHON_VERSION=$PYTHON_VERSION -DPYTHON_EXECUTABLE=/opt/python/$PYTHON/bin/python
-make -j $BUILD_THREADS bdist_wheel
-auditwheel repair $EXCLUDES pydip/staging/dist/*.whl
-
-# Python 3.11
-export PYTHON=cp311-cp311
-export PYTHON_VERSION=3.11
-$CMAKE .. -DPYBIND11_PYTHON_VERSION=$PYTHON_VERSION -DPYTHON_EXECUTABLE=/opt/python/$PYTHON/bin/python
-make -j $BUILD_THREADS bdist_wheel
-auditwheel repair $EXCLUDES pydip/staging/dist/*.whl
-
-# Python 3.12
-export PYTHON=cp312-cp312
-export PYTHON_VERSION=3.12
-$CMAKE .. -DPYBIND11_PYTHON_VERSION=$PYTHON_VERSION -DPYTHON_EXECUTABLE=/opt/python/$PYTHON/bin/python
-make -j $BUILD_THREADS bdist_wheel
-auditwheel repair $EXCLUDES pydip/staging/dist/*.whl
+# Build wheels
+for v in ${PYTHON_VERSIONS[@]}; do
+   $CMAKE .. -DPYBIND11_PYTHON_VERSION=$v -DPYTHON_EXECUTABLE=/opt/python/cp${v/./}-cp${v/./}/bin/python
+   make -j $BUILD_THREADS bdist_wheel
+   auditwheel repair ${EXCLUDES[@]/#/--exclude } pydip/staging/dist/*.whl
+done;
 
 mv wheelhouse /io
