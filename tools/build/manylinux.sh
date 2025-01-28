6bd0b881ba405831d1d61eff5ac4bb9a1eb7df5e
Cris Luengo <cris.l.luengo@gmail.com>
2025-01-28 09:18:06 -0700

    Fix METADATA file in Python wheels.

--- tools/build/manylinux.sh
+++ tools/build/manylinux.sh
@@ -3,8 +3,8 @@
 
 # Setup
 yum -y install freeglut-devel java-1.8.0-openjdk-devel.x86_64
-/opt/python/cp39-cp39/bin/python -m pip install cmake auditwheel
-CMAKE=/opt/python/cp39-cp39/lib/python3.9/site-packages/cmake/data/bin/cmake
+/opt/python/cp312-cp312/bin/python -m pip install cmake auditwheel
+CMAKE=/opt/python/cp312-cp312/lib/python3.12/site-packages/cmake/data/bin/cmake
 BUILD_THREADS=4
 PYTHON_VERSIONS=(3.10 3.11 3.12 3.13)
 EXCLUDES=(libjvm.so libOpenGL.so.0 libGLX.so.0 libGLdispatch.so.0)
@@ -19,7 +19,7 @@ $CMAKE .. -DDIP_PYDIP_WHEEL_INCLUDE_LIBS=On
 
 # Build wheels
 for v in ${PYTHON_VERSIONS[@]}; do
-   /opt/python/cp${v/./}-cp${v/./}/bin/python -m pip install setuptools wheel
+   /opt/python/cp${v/./}-cp${v/./}/bin/python -m pip install -U setuptools wheel packaging
    $CMAKE .. -DPython_EXECUTABLE=/opt/python/cp${v/./}-cp${v/./}/bin/python
    make -j $BUILD_THREADS bdist_wheel
    auditwheel repair ${EXCLUDES[@]/#/--exclude } pydip/staging/dist/*.whl
